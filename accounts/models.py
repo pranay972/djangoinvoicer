1e58490dac50213deb10d90529eb5da13c3f6f22
Aman Jain <jainaman224@gmail.com>
2017-09-18 20:43:46 +0530

    Inventroy update
    Signed-off-by: Aman Jain <jainaman224@gmail.com>

--- accounts/models.py
+++ accounts/models.py
@@ -5,93 +5,95 @@ from django.utils import timezone
 # Create your models here.
 
 class Product (models.Model):
-	name = models.CharField(max_length=100)
-	alias = models.CharField(max_length=100)
-	price = models.FloatField(validators=[MinValueValidator(0)])
-	inventory = models.IntegerField(default=0,
-									validators=[MinValueValidator(0)])
-	hsn_code = models.CharField(max_length=8, blank=True)
+    name = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100)
+    price = models.FloatField(validators=[MinValueValidator(0)])
+    inventory = models.IntegerField(default=0,
+                                    validators=[MinValueValidator(0)])
+    hsn_code = models.CharField(max_length=8, blank=True)
+    tax = models.FloatField(validators=[MinValueValidator(0)])
 
-	def __str__ (self):
-		return self.name
+
+    def __str__ (self):
+        return self.name
 
 class Bills (models.Model):
 
-	#constant
-	COLLECTION = (
-		("1", '(CGST/SGST)'),
-		("2", '(IGST)'),
-	)
+    #constant
+    COLLECTION = (
+        ("1", '(CGST/SGST)'),
+        ("2", '(IGST)'),
+    )
 
-	invoice_date = models.DateField(default=timezone.now())
-	to = models.CharField(max_length=100)
-	collection = models.CharField(max_length=100, choices=COLLECTION, blank=False, default=1)
+    invoice_date = models.DateField(default=timezone.now())
+    to = models.CharField(max_length=100)
+    collection = models.CharField(max_length=100, choices=COLLECTION, blank=False, default=1)
 
-	def tax(self):
-		return sum([((x.price*x.quantity)*int(x.category)/100) for x in self.billedproducts_set.all()])
+    def tax(self):
+        return sum([((x.price*x.quantity)*int(x.category)/100) for x in self.billedproducts_set.all()])
 
-	def total(self):
-		return sum([x.price*x.quantity for x in self.billedproducts_set.all()])
-	def tax_dict(self):
-		res = {
-			"5" : 0,
-			"12" : 0,
-			"18" : 0,
-			"28" : 0
-		}
-		for x in self.billedproducts_set.all():
-			res[str(x.category)] += (x.price*x.category*x.quantity)/100
-		return res
-	def price_tax_dict(self):
-		res = {
-			"5" : 0,
-			"12" : 0,
-			"18" : 0,
-			"28" : 0
-		}
-		for x in self.billedproducts_set.all():
-			res[str(x.category)] += (x.price*x.quantity)
-		return res
-	def totalQuantity(self):
-		return sum([int(x.quantity) for x in self.billedproducts_set.all()])
-	def grandTotal(self):
-		return sum([(x.price*x.quantity) + ((x.price*x.quantity)*int(x.category)/100) for x in self.billedproducts_set.all()])
+    def total(self):
+        return sum([x.price*x.quantity for x in self.billedproducts_set.all()])
+    def tax_dict(self):
+        res = {
+            "5" : 0,
+            "12" : 0,
+            "18" : 0,
+            "28" : 0
+        }
+        for x in self.billedproducts_set.all():
+            res[str(x.category)] += (x.price*x.category*x.quantity)/100
+        return res
+    def price_tax_dict(self):
+        res = {
+            "5" : 0,
+            "12" : 0,
+            "18" : 0,
+            "28" : 0
+        }
+        for x in self.billedproducts_set.all():
+            res[str(x.category)] += (x.price*x.quantity)
+        return res
+    def totalQuantity(self):
+        return sum([int(x.quantity) for x in self.billedproducts_set.all()])
+    def grandTotal(self):
+        return sum([(x.price*x.quantity) + ((x.price*x.quantity)*int(x.category)/100) for x in self.billedproducts_set.all()])
 
 class BilledProducts (models.Model):
 
-	#constants
-	CATEGORIES = (
-		(5, 5),
-		(12, 12),
-		(18, 18),
-		(28, 28),
-	)
+    #constants
+    CATEGORIES = (
+        (5, 5),
+        (12, 12),
+        (18, 18),
+        (28, 28),
+    )
 
-	name = models.CharField(max_length=100)
-	quantity = models.IntegerField(default=0)
-	price = models.FloatField()
-	bill = models.ForeignKey(Bills)
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+    price = models.FloatField()
+    bill = models.ForeignKey(Bills)
 
-	category = models.IntegerField(choices = CATEGORIES)
-	hsn = models.CharField(max_length=10)
+    category = models.IntegerField(choices = CATEGORIES)
+    hsn = models.CharField(max_length=10)
 
-	def taxTotal(self):
-		return ((self.price * self.quantity)*self.category)/100
+    def taxTotal(self):
+        return ((self.price * self.quantity)*self.category)/100
 
-	def total(self):
-		return self.price * self.quantity
+    def total(self):
+        return self.price * self.quantity
 
-	def grandTotal(self):
-		return self.price * self.quantity + ((self.price * self.quantity)*self.category)/100
+    def grandTotal(self):
+        return self.price * self.quantity + ((self.price * self.quantity)*self.category)/100
 
-	def __str__ (self):
-		return self.name
+    def __str__ (self):
+        return self.name
 
 class Tax(models.Model):
-	tax = models.FloatField(default=0)
+    tax = models.FloatField(default=0)
 
-	def __str__(self):
-		return self.tax
+    def __str__(self):
+        return self.tax
 
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
+from django.db.models import F
 from django.views.generic.edit import UpdateView
 
 from .forms import LoginForm, ProductAddForm, BillGenForm, TaxForm
@@ -25,133 +26,133 @@ def edit_product_view(request, id):
 #views
 @login_required
 def all_bill_view(request):
-	bills = Bills.objects.all().order_by('-invoice_date')
-	return render(request, 'accounts/all_bill.html', {'bills' : bills})
+    bills = Bills.objects.all().order_by('-invoice_date')
+    return render(request, 'accounts/all_bill.html', {'bills' : bills})
 
 
 @login_required
 def view_bill_view(request, id):
-	bill = get_object_or_404(Bills, id = id)
-	return render(request, 'accounts/view_bill.html', {'bill' : bill})
+    bill = get_object_or_404(Bills, id = id)
+    return render(request, 'accounts/view_bill.html', {'bill' : bill})
 
 
 @login_required
 def gen_bill_view(request):
-	form = BillGenForm()
-	data = []
-	for product in Product.objects.all():
-		data.append({
-			'name' : product.name,
-			'price' : product.price,
+    form = BillGenForm()
+    data = []
+    for product in Product.objects.all():
+        data.append({
+            'name' : product.name,
+            'price' : product.price,
             'id' : product.id,
             'inventory' : product.inventory,
             'hsn_code' : product.hsn_code
-			})
-	import json
-	data = json.dumps(data)
-	return render(request, 'accounts/bill_gen.html', {'form':form, 'data' : data})
+        })
+    import json
+    data = json.dumps(data)
+    return render(request, 'accounts/bill_gen.html', {'form':form, 'data' : data})
 
 
 @login_required
 def products_view(request, view_data = {}):
 
-	#getting the products
-	products = Product.objects.all()
-	view_data['products'] = products
+    #getting the products
+    products = Product.objects.all()
+    view_data['products'] = products
 
-	#getting the products add form
-	product_add_form = ProductAddForm(view_data.get('form_data'))
-	view_data['product_add_form'] = product_add_form
+    #getting the products add form
+    product_add_form = ProductAddForm(view_data.get('form_data'))
+    view_data['product_add_form'] = product_add_form
 
-	addition = ""
+    addition = ""
 
-	if view_data.get('group_success'):
-		addition = '#groups'
+    if view_data.get('group_success'):
+        addition = '#groups'
 
-	return render(request, 'accounts/products.html'+addition, view_data)
+    return render(request, 'accounts/products.html'+addition, view_data)
 
 
 @login_required
 def index(request):
 
-	if request.method == "POST":
-		form = TaxForm(request.POST)
-		if form.is_valid():
-			if Tax.objects.count() > 0:
-				tax = Tax.objects.all()[0]
-				tax.tax = form.cleaned_data['tax']
-				tax.save()
-			else:
-				tax = Tax.objects.create(tax = form.cleaned_data['tax'])
-				tax.save()
+    if request.method == "POST":
+        form = TaxForm(request.POST)
+        if form.is_valid():
+            if Tax.objects.count() > 0:
+                tax = Tax.objects.all()[0]
+                tax.tax = form.cleaned_data['tax']
+                tax.save()
+            else:
+                tax = Tax.objects.create(tax = form.cleaned_data['tax'])
+                tax.save()
 
-	#get product count
-	pc = Product.objects.count()
+    #get product count
+    pc = Product.objects.count()
 
-	#bills count
-	bc = Bills.objects.count()
+    #bills count
+    bc = Bills.objects.count()
 
-	instance = None
-	if Tax.objects.count() > 0:
-		instance = Tax.objects.all()[0]
+    instance = None
+    if Tax.objects.count() > 0:
+        instance = Tax.objects.all()[0]
 
-	#tax form
-	tax_form = TaxForm(instance = instance)
+    #tax form
+    tax_form = TaxForm(instance = instance)
 
-	return render(request, 'accounts/index.html', {'bill_count' : bc, 'product_count':pc})
+    return render(request, 'accounts/index.html', {'bill_count' : bc, 'product_count':pc})
 
 
 def logout_view(request):
-	if request.user.is_authenticated():
-		from django.contrib.auth import logout
-		logout(request)
-		return HttpResponseRedirect('/login')
-	else:
-		raise Http404()
+    if request.user.is_authenticated():
+        from django.contrib.auth import logout
+        logout(request)
+        return HttpResponseRedirect('/login')
+    else:
+        raise Http404()
 
 
 def login_view(request):
-	if request.user.is_authenticated():
-		return HttpResponseRedirect("/")
+    if request.user.is_authenticated():
+        return HttpResponseRedirect("/")
 
-	invalid = False
-	if request.method == 'POST':
-		form = LoginForm(request.POST)
+    invalid = False
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
 
-		if form.is_valid():
-			username = form.cleaned_data['user']
-			password = form.cleaned_data['password']
-			from django.contrib.auth import authenticate, login
-			user = authenticate(username = username, password = password)
+        if form.is_valid():
+            username = form.cleaned_data['user']
+            password = form.cleaned_data['password']
+            from django.contrib.auth import authenticate, login
+            user = authenticate(username = username, password = password)
 
-			if user is not None:
-				login(request, user)
+            if user is not None:
+                login(request, user)
 
-				try:
-					return HttpResponseRedirect(request.GET['next'])
-				except:
-					return HttpResponseRedirect('/')
+                try:
+                    return HttpResponseRedirect(request.GET['next'])
+                except:
+                    return HttpResponseRedirect('/')
 
-			else:
-				invalid = True
+            else:
+                invalid = True
 
-	else:
+    else:
 
-		form = LoginForm()
+        form = LoginForm()
 
-	return render(request, 'accounts/login.html', {'form' : form, 'invalid': invalid})
+    return render(request, 'accounts/login.html', {'form' : form, 'invalid': invalid})
 
 
 # helpers
 @login_required
 def del_product_helper(request, id):
-	if request.method == "POST":
-		product = Product.objects.get(pk=id)
-		product.delete()
-		return redirect('/products', {'product_success' : True})
+    if request.method == "POST":
+        product = Product.objects.get(pk=id)
+        product.delete()
+        return redirect('/products', {'product_success' : True})
 
-	else:
-		raise Http404()
+    else:
+        raise Http404()
 
 
 @login_required
@@ -178,54 +179,55 @@ def add_product_helper(request):
 
 # apis
 def gen_bill(request):
-	if request.method == "POST":
-		form = BillGenForm(request.POST)
-		if form.is_valid():
-			bill = Bills.objects.create(
-				invoice_date = form.cleaned_data['invoice_date'],
-				to = form.cleaned_data['to'],
-				collection = form.cleaned_data['collection']
-			)
+    if request.method == "POST":
+        form = BillGenForm(request.POST)
+        if form.is_valid():
+            bill = Bills.objects.create(
+                invoice_date = form.cleaned_data['invoice_date'],
+                to = form.cleaned_data['to'],
+                collection = form.cleaned_data['collection']
+            )
 
-			import json
+            import json
 
-			billed = json.loads(request.POST['names_gst_and_quantities'])
-			billed_list = []
-			for x in billed:
-				billed_product = BilledProducts.objects.create(
-					name = x['name'],
-					price = x['price'],
-					quantity = x['quantity'],
-					category = int(x['category']),
-					hsn = x['hsn'],
-					bill = bill
-				)
-				billed_list.append(billed_product)
+            billed = json.loads(request.POST['names_gst_and_quantities'])
+            billed_list = []
+            for x in billed:
+                billed_product = BilledProducts.objects.create(
+                    name = x['name'],
+                    price = x['price'],
+                    quantity = x['quantity'],
+                    category = int(x['category']),
+                    hsn = x['hsn'],
+                    bill = bill
+                )
+                billed_list.append(billed_product)
+                print(Product.objects.filter(pk=x['id']).update(
+                    inventory=F('inventory') - x['quantity']))
+                print(each.inventory for each in Product.objects.filter(pk=x['id']))
+            bill.save()
+            for b in billed_list:
+                b.save()
+            return HttpResponse("success#"+str(bill.pk))
 
+        else:
+            return HttpResponse(str(form.errors))
 
-			bill.save()
-			for b in billed_list:
-				b.save()
-			return HttpResponse("success#"+str(bill.pk))
-
-		else:
-			return HttpResponse(str(form.errors))
-
-	else:
-		raise Http404()
+    else:
+        raise Http404()
 
 
 def search_product(request, query):
-	if request.method == "POST":
-		data = []
-		for product in Product.objects.filter(name__icontains = query):
-			data.append({
-				'name' : product.name,
-				'alias' : product.alias,
-				'price' : product.price
-				})
-		import json
-		data = json.dumps(data)
-		return HttpResponse(data)
-	else:
-		raise Http404()
+    if request.method == "POST":
+        data = []
+        for product in Product.objects.filter(name__icontains = query):
+            data.append({
+                'name' : product.name,
+                'alias' : product.alias,
+                'price' : product.price
+            })
+        import json
+        data = json.dumps(data)
+        return HttpResponse(data)
+    else:
+        raise Http404()
